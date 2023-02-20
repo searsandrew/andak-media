2920570b49b1d379374ea9a305738c69c1b95619
Andrew Sears <andrewsears@Sandman-Air.cmp.local>
2023-02-20 16:47:52 -0600

    Updates to welcoem screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,16 +2,10 @@
     <div class="py-12">
         <div class="flex flex-row max-w-7xl mx-auto sm:px-6 lg:px-8">
             @forelse(App\Models\News::all() as $news)
-                <span class="w-full px-2 py-3 sm:w-1/4 h-1/3 bg-white overflow-hidden shadow-xl sm:rounded">
-                    <h2 class="text-slate-700 lighter text-lg antialiased">{{ $news->title }}</h2>
-                    <img src="{{ asset($news->image->image) }}" />
-
-                    @if(File::exists($news->image->image))
-                        Fuck yes
-                    @else
-                        Fuck No
-                    @endif
-                </span>
+                <a href="#" class="w-full sm:w-1/4 h-52 bg-white overflow-hidden shadow-xl sm:rounded relative flex flex-col">
+                    <h2 class="text-slate-700 lighter text-lg antialiased 8-8 relative z-30 p-5 text-white bg-gray-800 hover:text-slate-800 hover:bg-andakTeal bg-opacity-50 transition">{{ $news->title }}</h2>
+                    <img src="{{ Storage::url($news->image->image) }}" class="absolute z-10 w-auto min-w-full min-h-full max-w-full" />
+                </a>
             @empty
                 <span class="text-slate-800 lighter text-lg antialiased">{{ __('Sadly, there is no news to report.') }}</span>
             @endforelse
