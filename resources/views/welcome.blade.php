7b3d6e63133c858ac9822c5dffc0620ba0c772da
Andrew Sears <andrewsears@Sandman-Air.cmp.local>
2023-02-20 15:37:05 -0600

    Storage Link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
             @forelse(App\Models\News::all() as $news)
                 <span class="w-full px-2 py-3 sm:w-1/4 h-1/3 bg-white overflow-hidden shadow-xl sm:rounded">
                     <h2 class="text-slate-700 lighter text-lg antialiased">{{ $news->title }}</h2>
-                    <img src="" />
+                    <img src="{{ asset($news->image->image) }}" />
+                    {{ Storage::url($news->image->image) }}
                 </span>
             @empty
                 <span class="text-slate-800 lighter text-lg antialiased">{{ __('Sadly, there is no news to report.') }}</span>
